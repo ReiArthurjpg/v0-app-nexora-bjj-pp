241b2ac586e53cb6bd23388e1d3ab115e6bcfd2c
ReiArthurjpg <arthurnexora@gmail.com>
2026-04-09 21:23:33 -0300

    Atualização de page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Lpcontroller from '@/feature/guest/site/controller/Lpcontroller'
+import { Lpcontroller } from '@/feature/guest/site'
 
 export default function Home() {
   return <Lpcontroller />
